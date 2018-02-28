2f5fd15ed7a4920c7f92f5b8c1b9f72a58b48caf
TAKAHIRO KINEBUCHI <like.takahiro@gmail.com>
2018-02-28 18:20:17 +0900

    5の2

--- Practice/src/practice05/PTra05_02.java
+++ Practice/src/practice05/PTra05_02.java
@@ -20,6 +20,8 @@ public class PTra05_02 {
 		// ★ 変数inputを宣言して、変数lineを数値に変換した値を代入してください
 		// ※ コマンドプロンプトで入力された値が、数字ではなかった場合はプログラムがエラーになって良いです
 
+		int input;
+		input = Integer.parseInt(line);
 
 		/*
 		 *  ★ 以下の仕様で、switch文を記述してください
@@ -30,5 +32,12 @@ public class PTra05_02 {
 		 *  ※##は変数inputの中身
 		 */
 
+		switch(input%2) {//式を入れても問題なし。変数に指定できるのは整数、文字列、文字のみ
+		case 0:
+			System.out.println(input + "は偶数です");
+			break;
+		case 1:
+			System.out.println(input + "は奇数です");
+		}
 	}
 }
