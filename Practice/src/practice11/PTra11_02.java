58b519aa5300fe20a90eb5dbe966f3acc7b040da
TAKAHIRO KINEBUCHI <like.takahiro@gmail.com>
2018-03-09 12:11:20 +0900

    11の2

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -1,20 +1,40 @@
 package practice11;
-/*
- * PTra11_02.java
- *   作成	LIKEIT	2017
- *------------------------------------------------------------
- * Copyright(c) Rhizome Inc. All Rights Reserved.
- */
 
 public class PTra11_02 {
 	public static void main(String[] args) {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
 
+		Book[] bookData = FileReaderClass.readBookDataFile();
+
+
+
+		// 入力型プログラムです。
+		// 下記の命令は、入力型プログラムで必要な記述になります。
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
 
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
 
+		// コマンドプロンプトで入力した文字列が変数lineに代入されます
+		String line = scanner.nextLine();
+
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
+
+		for(int i = 0;i < bookData.length;i++) {
+			if(bookData[i].title.indexOf(line)!=-1) {
+				System.out.println(bookData[i].dispBookInfo());
+			}
+
+
+		}
+
+//		if(Arrays.asList(bookData).contains(line)) {
+//			System.out.println(bookData);
+//		}else {
+//			System.out.println("再検索してください");
+//		}
+
+
 
 	}
 }
