44faad201f17bf8f6589c38b936883c6b2e65cc8
TAKAHIRO KINEBUCHI <like.takahiro@gmail.com>
2018-03-12 16:04:01 +0900

    practice13の3

--- Practice/src/practice13/ptra13/PTra13_03.java
+++ Practice/src/practice13/ptra13/PTra13_03.java
@@ -6,6 +6,8 @@
  */
 package practice13.ptra13;
 
+import practice13.common.Slime;
+
 public class PTra13_03 {
 
 	/*
@@ -13,14 +15,18 @@ public class PTra13_03 {
 	 * 	●HP, POWER, ENDURANCEはそれぞれ、（10, 5, 2）
 	 */
 
+
 	public static void main(String[] args) {
 		// ★ common.Slimeクラスをインスンタンス化してください
 
+		Slime slime = new Slime();
 
 		// ★ common.Slimeインスタンスのnameに"スライム"を設定して下さい
 
+		slime.setName("スライム");
 
 		// ★ common.Slimeインスタンスのキャラクター情報を表示してください
 
+		System.out.println(slime.showParameter());
 	}
 }
