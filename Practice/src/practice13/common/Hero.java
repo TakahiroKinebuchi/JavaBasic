d88c14c807a83ae3ee02cbfdc6d653ad19d222ac
TAKAHIRO KINEBUCHI <like.takahiro@gmail.com>
2018-03-12 16:02:31 +0900

    practice13のcommonパッケージ内のHero

--- Practice/src/practice13/common/Hero.java
+++ Practice/src/practice13/common/Hero.java
@@ -2,9 +2,8 @@ package practice13.common;
 
 public class Hero extends Character{
 
-		Hero(int hp, int power, int endurance) {
+		public Hero() {
 			super(25, 10, 7);
-			// TODO 自動生成されたコンストラクター・スタブ
 		}
 
 }
