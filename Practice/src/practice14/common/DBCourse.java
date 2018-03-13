7af8bebdfe236b0bb9de4440c30b9d57c3013520
TAKAHIRO KINEBUCHI <like.takahiro@gmail.com>
2018-03-13 17:02:18 +0900

    14のcommonパッケージ内のDBCourseクラス

---
+++ Practice/src/practice14/common/DBCourse.java
@@ -0,0 +1,12 @@
+package practice14.common;
+
+public class DBCourse implements Course{
+	public String getCourseName() {
+		return PREFIX + "DB基礎";
+	}
+
+	public String[] getCourseUnit() {
+		String[] result = {"DB基礎","SQL基礎","正規化","SQL応用"};
+		return result;
+	}
+}
