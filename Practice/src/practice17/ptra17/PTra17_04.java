55c92e239713c26d79085aa834ea6f1008fec63d
TAKAHIRO KINEBUCHI <like.takahiro@gmail.com>
2018-03-15 16:00:36 +0900

    17の3、17の4

--- Practice/src/practice17/ptra17/PTra17_04.java
+++ Practice/src/practice17/ptra17/PTra17_04.java
@@ -16,5 +16,19 @@ public class PTra17_04 {
 		 * ※ Thread.sleep を使用すること
 		 */
 
+		//文字列を配列に一文字ずつセット
+		char data[] = str.toCharArray();
+
+		//配列数を取得
+		int arr_num = data.length;
+
+		//i = 0 ~ i = 12 の間繰り返しを行う。ex.data[0] = "ス"・・・以下同様
+		for(int i = 0;i <= arr_num-1; i++) {
+			try {
+				Thread.sleep(1000);//10秒間眠るThread.sleep(millisecond)
+			}catch(InterruptedException e) {
+			}
+			System.out.print(data[i]);
+		}
 	}
 }
