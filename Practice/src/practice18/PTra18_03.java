e0f8c29c1af8cc5092ba8e6a38d1d770f35174b0
TAKAHIRO KINEBUCHI <like.takahiro@gmail.com>
2018-03-16 17:46:32 +0900

    18の3

--- Practice/src/practice18/PTra18_03.java
+++ Practice/src/practice18/PTra18_03.java
@@ -6,6 +6,14 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import practice18.entity.Player;
+
 public class PTra18_03 {
 
 	/*
@@ -20,12 +28,45 @@ public class PTra18_03 {
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
 
+		ArrayList<Player> array = new ArrayList<Player>();
+
+		try (Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+
+			while (scanner.hasNext()) {
+				String line = scanner.nextLine();
+				String[] splitLine = line.split(",", 0);
+
+				Player list = new Player();
+
+				list.setPosition(splitLine[0]);
+				list.setName(splitLine[1]);
+				list.setCountry(splitLine[2]);
+				list.setTeam(splitLine[3]);
+
+				array.add(list);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("ファイルが見つかりませんでした");
+		}
 
 		// ★ ①のArrayListの中から"レアル・マドリード", "バルセロナ"の選手を除外してください
 
+		//arrayの中にある各list、1つ1つチェックして"レアル・マドリード"か"バルセロナ"がequals(String)だったら、除外
+
+		Iterator<Player> i = array.iterator();
+		while (i.hasNext()) {
+			Player members = i.next();
+
+			if (members.getTeam().equals("レアル・マドリード") || members.getTeam().equals("バルセロナ")) {
+				i.remove();
+			}
+		}
 
 		// ★ 削除後のArrayListの中身を全件出力してください
 
+		for (Player toString : array) {
+			System.out.println(toString);
+		}
 
 	}
 }
