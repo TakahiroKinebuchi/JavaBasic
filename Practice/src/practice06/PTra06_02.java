499a17a69c72803d3a59a907e04b8c8f20ecd9a2
TAKAHIRO KINEBUCHI <like.takahiro@gmail.com>
2018-03-19 17:02:10 +0900

    6の2

--- Practice/src/practice06/PTra06_02.java
+++ Practice/src/practice06/PTra06_02.java
@@ -12,17 +12,17 @@ public class PTra06_02 {
 		// ★ 20回繰り返しを行うfor文を記述してください
 		//-----for start
 
-		for(int i = 1; i <= 20; i++) {
+		for(int i = 0; i < 20; i++) {
 			// ★ 今何回目の処理なのかを出力してください
 
-			System.out.println("今" + i + "回目の処理です");
+			System.out.println("今" + (i+1) + "回目の処理です");
 
 			// ★ 奇数回のときには、「奇数」を出力してください
 
-			if(i%2 == 0) {
-				continue;
+			if((i+1)%2 == 1) {
+				System.out.println("奇数");
 			}
-			System.out.println("奇数");
+
 		}
 
 
