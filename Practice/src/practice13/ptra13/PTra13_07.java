150c2dd194f59058cdffd9973fab9838f3a37e74
TAKAHIRO KINEBUCHI <like.takahiro@gmail.com>
2018-03-20 14:19:38 +0900

    13の7

--- Practice/src/practice13/ptra13/PTra13_07.java
+++ Practice/src/practice13/ptra13/PTra13_07.java
@@ -24,17 +24,13 @@ public class PTra13_07 {
 		SuperHero SuperHero = new SuperHero();
 		// 10, 5, 2
 		Slime Slime = new Slime();
-
-
 		SuperHero.setName("勇者（装備あり）");
 		Slime.setName("スライム");
-		// ★ Itemクラスのインスタンスを作成し、("こんぼう", 4）をコンストラクタの引数にしてください
 
+		// ★ Itemクラスのインスタンスを作成し、("こんぼう", 4）をコンストラクタの引数にしてください
 		Item Item = new Item("こんぼう", 4);
 
 		// ★ 作成したItemインスタンスをSuperHeroに持たせてください
-
-
 		SuperHero.setEquipment(Item);
 
 		/*
@@ -45,25 +41,17 @@ public class PTra13_07 {
 		 */
 
 		while(true) {
-
 			boolean isSlimeDead = Slime.damage(SuperHero.attack());
-
 			if(isSlimeDead) {
 				System.out.println(SuperHero.getName() + "は" + Slime.getName() + "との戦闘に勝利した");
 				break;
 			}
-
 			boolean isHeroDead = SuperHero.damage(Slime.attack());
-
 			if(isHeroDead) {
 				System.out.println(Slime.getName() + "は" + SuperHero.getName() + "との戦闘に勝利した");
 				break;
 			}
-
-
 		// ★ 勝利した方の出力を行ってください。「○○は■■との戦闘に勝利した」
 		}
 	}
-
-
 }
